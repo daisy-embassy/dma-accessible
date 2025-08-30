916375b09132789e6d380c24ba73267c86e2046c
Dickless <011001nobuo@gmail.com>
2025-08-30 14:18:59 +0900

    Remove tests module

--- src/lib.rs
+++ src/lib.rs
@@ -167,20 +167,3 @@ impl<T: Copy, const LEN: usize, Region: DmaAccessible> DmaBuffer<T, LEN, Region>
         self.ptr.as_ptr()
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use grounded::uninit::GroundedArrayCell;
-
-    use crate::{DmaBuffer, Sram1};
-
-    // Since there's no way to link to specific memory regions in a std environment,
-    // the test is expected to panic, and I wanted to check if it builds and if it
-    // can be combined with GroundedArrayCell.
-    #[should_panic(expected = "Buffer not in DMA-accessible region")]
-    #[test]
-    fn test() {
-        static BUFFER: GroundedArrayCell<u8, 128> = GroundedArrayCell::uninit();
-        let _da = DmaBuffer::<u8, 128, Sram1>::new(&BUFFER, 0);
-    }
-}
