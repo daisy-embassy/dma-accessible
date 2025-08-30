2f80e274659c5bf877a08bc556eabad28fe6e579
Dickless <011001nobuo@gmail.com>
2025-08-30 12:27:01 +0900

    Update configuration and dependencies to introduce defmt for loggin during test

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,17 +1,23 @@
 #![no_std]
 #![no_main]
 use cortex_m_rt::entry;
+use defmt::info;
 use dma_accessible::{DmaBuffer, Sram1};
 use grounded::uninit::GroundedArrayCell;
-use panic_halt as _;
+
+use defmt_rtt as _;
+use panic_probe as _;
 
 #[entry]
 fn main() -> ! {
+    info!("Hello, basic test!");
     test_dma_buffer_creation();
+    info!("test has completed. no problem!!");
     loop {}
 }
 
 fn test_dma_buffer_creation() {
+    info!("test_dma_buffer_creation");
     // Buffer must be placed in a DMA-accessible region (e.g., SRAM1)
     #[unsafe(link_section = ".sram1_bss")]
     static BUFFER: GroundedArrayCell<u8, 1024> = GroundedArrayCell::uninit();
